61f75be6b1d70b783888b6a79f433dd205aef9af
Tim McWilliams <tmcwilliams1127@gmail.com>
2019-03-04 12:20:19 -0500

    updating the plots

--- StatsHelper/plots.py
+++ StatsHelper/plots.py
@@ -59,21 +59,20 @@ def corr_matrix(df, fig_filename):
                 square = True, linewidths = .5, cbar_kws = {"shrink": 1})
     f.savefig(fig_filename)
  
-def histogram(data, n_bins, cumulative = False, x_label, y_label, title, color, fig_filename):
+def histogram(data, n_bins, x_label, y_label, title, color, fig_filename):
     '''
     Generates a histogram 
     data <- the df
     n_bins <- how many discrete bins we want for our histogram
-    cumulative <- a boolean which allows us to select whether our histogram is cumulative or not
     x_label <- the x-label 
     y_label <- the y-label 
     title <- the title of the plot
     fig_filename <- the name of the file that you want to save the figure as
     '''
     # Create the plot object
-    _, ax = plt.subplots()
+    ax = plt.subplot()
     # Plot the data
-    ax.hist(data, n_bins = n_bins, cumulative = cumulative, color = color)
+    ax.hist(data, n_bins = n_bins, color = color)
     # Label the axes and provide a title
     ax.set_ylabel(y_label)
     ax.set_xlabel(x_label)
@@ -81,18 +80,14 @@ def histogram(data, n_bins, cumulative = False, x_label, y_label, title, color, 
     # Save the fig
     plt.savefig(fig_filename)
 
-def overlaid_histogram(data1, data2, n_bins = 0, data1_name, data1_color, data2_name, data2_color, x_label, y_label, title, fig_filename):
+def overlaid_histogram(data1, data2, n_bins, data1_name, data2_name, title, fig_filename):
     '''
     Generates a overlaid histogram (2 variables)
     data1 <- the first variable 
     data2 <- the first variable 
     n_bins <- how many discrete bins we want for our histogram
-    data1_color <- the color of variable 1
-    data2_color <- the color of variable 2
     data1_name <- label the graph with variable 1's name 
     data2_name <- label the graph with variable 2's name
-    x_label <- the x-label 
-    y_label <- the y-label 
     title <- the title of the plot
     fig_filename <- the name of the file that you want to save the figure as
     '''
@@ -101,17 +96,17 @@ def overlaid_histogram(data1, data2, n_bins = 0, data1_name, data1_color, data2_
     data_range = [min(min(data1), min(data2)), max(max(data1), max(data2))]
     binwidth = (data_range[1] - data_range[0]) / max_nbins
     # If the bins are 0, create bins, else bins are 0
-    if n_bins == 0
+    if n_bins == 0:
         bins = np.arange(data_range[0], data_range[1] + binwidth, binwidth)
     else: 
         bins = n_bins
     # Create the plot
-    _, ax = plt.subplots()
-    ax.hist(data1, bins = bins, color = data1_color, alpha = 1, label = data1_name)
-    ax.hist(data2, bins = bins, color = data2_color, alpha = 0.75, label = data2_name)
+    ax = plt.subplot()
+    ax.hist(data1, bins = bins, color = 'lightblue', alpha = 1, label = data1_name)
+    ax.hist(data2, bins = bins, color = 'green', alpha = 0.75, label = data2_name)
     # Label the axis's
-    ax.set_ylabel(y_label)
-    ax.set_xlabel(x_label)
+    ax.set_ylabel('Frequency')
+    ax.set_xlabel('Observations')
     ax.set_title(title)
     ax.legend(loc = 'best')
     # Save the fig
@@ -125,7 +120,7 @@ def kde_plot(df, var, title, xlabel, fig_filename):
     fig = kde.get_figure()
     fig.savefig(fig_filename)
 
-def scatterplot(x_data, y_data, x_label, y_label, title, color, s, alpha, yscale_log = False, fig_filename):
+def scatterplot(x_data, y_data, x_label, y_label, title, color, s, alpha, fig_filename):
     ''' 
     Generates a scatter plot 
     x_data <- first variable 
@@ -140,12 +135,9 @@ def scatterplot(x_data, y_data, x_label, y_label, title, color, s, alpha, yscale
     fig_filename <- the name of the file that you want to save the figure as 
     '''
     # Create the plot object
-    _ , ax = plt.subplots()
+    ax = plt.subplot()
     # Plot the data
     ax.scatter(x_data, y_data, s = s, color = color, alpha = alpha)
-    # If yscale_log is true then scale the y-axis
-    if yscale_log == True:
-        ax.set_yscale('log')
     # Label the axes and provide a title
     ax.set_title(title)
     ax.set_xlabel(x_label)
@@ -167,7 +159,7 @@ def lineplot(x_data, y_data, x_label, y_label, title, color, lw, alpha, fig_file
     fig_filename <- the name of the file that you want to save the figure as 
     '''
     # Create the plot object
-    _ , ax = plt.subplots()
+    ax = plt.subplot()
     # Plot the best fit line, set the line-width (lw), color and
     # transparency (alpha) of the line
     ax.plot(x_data, 
@@ -183,7 +175,7 @@ def lineplot(x_data, y_data, x_label, y_label, title, color, lw, alpha, fig_file
     plt.savefig(fig_filename)
 
 def boxplot(x_data, y_data, base_color, median_color, x_label, y_label, title, fig_filename):
-   ''' 
+    ''' 
     Generates a box plot
     x_data <- variables for box 
     y_data <- values for the boxes 
@@ -194,13 +186,13 @@ def boxplot(x_data, y_data, base_color, median_color, x_label, y_label, title, f
     median_color <- the color of the median bar
     fig_filename <- the name of the file that you want to save the figure as 
     '''
-   # Create the plot object
-    _ , ax = plt.subplots()
+    # Create the plot object
+    ax = plt.subplot()
     # Draw box-plots, specifying desired style
     ax.boxplot(y_data,
                patch_artist = True,
                medianprops = {'color': median_color},
-               boxprops = {'color': base_color'facecolor': base_color},
+               boxprops = {'color': base_color, 'facecolor': base_color},
                whiskerprops = {'color': base_color},
                capprops = {'color': base_color})
 
